ff75a6eff188e9fbf672c9e7c7dfd99af58f9d05
Zack Klinger <zklinger@gmail.com>
2015-06-01 16:21:27 -0700

    ADD this1.js example of how 'this' is used in JavaScript

---
+++ this1.js
@@ -0,0 +1,10 @@
+//simple example how this in bas() is referring to foo()
+var foo = {
+	bar: 123,
+	bas: function () {
+		console.log('inside this.bar is: ', this.bar);
+	}
+}
+
+console.log('foo.bar is: ', foo.bar); //foo.bar is: 123
+foo.bas(); //inside this.bar is: 123
