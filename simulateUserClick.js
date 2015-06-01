6a6702ddda6e66a6efd9d46e2edfee633d32d749
Zack Klinger <zklinger@gmail.com>
2015-06-01 13:01:48 -0700

    RENAME app.js to simulateUserClick.js and commented the code' '

--- simulateUserClick.js
+++ simulateUserClick.js
@@ -1,3 +1,7 @@
+//This simulates how the callback is used to handle the waiting
+//for a response from a server
+//This is displaying the first-class functionality of JavaScript
+//I think this means you can pass a function in as a parameter
 function longRunningOperation(callback) {
 	// simulate a 3 second operation
 	setTimeout(callback, 3000);
