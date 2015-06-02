9c65b3be0351984f440fa0939133c1b2ed2f0e66
Zack Klinger <zklinger@gmail.com>
2015-06-02 15:01:29 -0700

    Final example from chapter

--- chapter-03-amd/client/app.js
+++ chapter-03-amd/client/app.js
@@ -1,1 +1,8 @@
-console.log('Hello requirejs!');
+//To be upfront about all the modules we need in a file, the root of the
+//file contains a call to define. To load modules ./foo and ./bar in app.js
+//in the same folder, the define call will be as shown
+define(['./foo', './bar'], function (foo, bar) {
+	//use foo and bar here
+	foo();
+	bar.log();
+});//END define
