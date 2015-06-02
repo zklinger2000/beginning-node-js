0f69a034570fadb444f9ad53b3fe7b0292c77921
Zack Klinger <zklinger@gmail.com>
2015-06-01 17:10:33 -0700

    ADD class.js as the preferred class pattern for Node.js

---
+++ class.js
@@ -0,0 +1,20 @@
+//This is the preferred pattern for the Node.js classes
+//
+//Class definition
+function someClass() {
+	//Properties go here
+	this.someProperty = 'some initial value';
+}
+//Member functions go here:
+someClass.prototype.someMemberFunction = function () {
+	/* Do something using this */
+	this.someProperty = 'modified value';
+}
+
+//Creation
+var instance = new someClass();
+
+//Usage
+console.log(instance.someProperty); //some initial value
+instance.someMemberFunction();
+console.log(instance.someProperty); //modified value
