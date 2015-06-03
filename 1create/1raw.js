7408c720aebe96f4c56737fe4eee9ca214dfee99
Zack Klinger <zklinger@gmail.com>
2015-06-03 16:10:41 -0700

    ADD 1create/1raw.js to show a very simple http module with a response and listener

---
+++ 1create/1raw.js
@@ -0,0 +1,12 @@
+//loading in the http module
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+	console.log('request starting...');
+	//respond
+	response.write('hello client!');
+	response.end();
+});
+//start event handler/listener on port 3000
+server.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
